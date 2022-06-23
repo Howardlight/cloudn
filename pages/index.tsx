c91ff1db4696a9798e9e46be54fdbd50ee35d3d5
HowardLight <howardlight@protonmail.com>
2022-06-24 01:13:56 +0300

    Created Background Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -132,4 +132,9 @@ const WeatherWidget = ({dayForcast}: {dayForcast: DayForcast}) => {
     );
 }
 
+//TODO: Find a type for this, check Cheatsheet
+const Background = (props: any) => {
+    return <div className="bg-fixed" style={{backgroundImage: `url("${bg.src}")`}} >{props.children}</div>
+}
+
 export default Home
