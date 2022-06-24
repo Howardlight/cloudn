54dda66ccc9f8cb0e53e1ead48e7ef4a68c40165
HowardLight <howardlight@protonmail.com>
2022-06-24 22:34:10 +0300

    Imported Header and Background in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Header from "../components/Header";
 import {Fragment} from "react";
+import Header from '../components/Header';
+import Background from "../components/Background";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Fragment>
-    <Header />
-    <Component {...pageProps} />
+    <Background>
+      <Header />
+      <Component {...pageProps} />
+    </Background>
   </Fragment>
 }
 
