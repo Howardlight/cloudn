e41a273044f4e6d4598ddfe09a96899405900fb2
HowardLight <howardlight@protonmail.com>
2022-06-25 12:28:44 +0300

    Added functioning Service Worker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,31 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import {Fragment} from "react";
+import {Fragment, useEffect} from "react";
 import Header from '../components/Header';
 import Background from "../components/Background";
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+
+    // Service Worker
+    if("serviceWorker" in navigator) {
+      window.addEventListener("load", function() {
+        navigator.serviceWorker.register("/sw.js").then(
+            function(registration) {
+              console.log("Service Worker registration success");
+            },
+            function (err) {
+              console.log("Service Worker registration failed:", err);
+            }
+        )
+      })
+    }
+
+  }, []);
+
+
+
   return <Fragment>
     <Background>
       <Header />
