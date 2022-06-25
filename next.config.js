f9830cf489f29a804e8e97fddb6fd66f24b12203
HowardLight <howardlight@protonmail.com>
2022-06-25 23:48:09 +0300

    removed swSRC, changed disable to only enable sw in prod

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   pwa: {
     dest: "public",
-    disable: process.env.NODE_ENV === 'development', //allows testing in local, REMOVE THIS IN PROD
-    swSrc: "service-worker.js"
-  }
+    disable: false, //allows testing in local, REMOVE THIS IN PROD
+  },
+
 }
 
 module.exports = withPWA(nextConfig);
