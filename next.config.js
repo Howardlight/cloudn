691f43d8825c659babbf45d0a047b0db3b07e7ff
HowardLight <howardlight@protonmail.com>
2022-06-27 12:35:38 +0300

    Disabled Service Workers

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
 
+//TODO: the service worker requests the api with lon/lat = 0,
+// Find a way to get location through other means
+// FOR NOW SERVICE WORKERS WILL BE DISABLED
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   pwa: {
     dest: "public",
-    disable: process.env.NODE_ENV === 'development', //allows testing in local, REMOVE THIS IN PROD
+    disable: true, //allows testing in local, REMOVE THIS IN PROD
+    // disable: process.env.NODE_ENV === 'development', //allows testing in local, REMOVE THIS IN PROD
   },
 
 }
