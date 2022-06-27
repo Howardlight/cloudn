cf05dffefd4898ae7ea7c33093066a02a22a8352
HowardLight <howardlight@protonmail.com>
2022-06-27 23:32:05 +0300

    Improved Header CSS

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,14 +3,13 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 
 //TODO: Come back to this and Improve CSS
 const Header = () => {
     return(
         <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static" className="bg-transparent backdrop-blur-sm shadow-transparent">
+            <AppBar className="static bg-transparent backdrop-blur-sm shadow-transparent">
                 <Toolbar>
                     <Typography
                         variant="h6"
@@ -20,7 +19,7 @@ const Header = () => {
                         className="color-white opacity-75"
                         sx={{
                             mr: 2,
-                            display: { xs: 'flex', md: 'flex' },
+                            display: "flex",
                             fontFamily: 'monospace',
                             fontWeight: 700,
                             letterSpacing: '.3rem',
