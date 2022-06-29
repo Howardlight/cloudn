52763392d3c0f28f872f6b7ebd54810ea990769d
HowardLight <howardlight@protonmail.com>
2022-06-29 12:47:12 +0300

    see prev commit

---
+++ pages/about.tsx
@@ -0,0 +1,22 @@
+import { NextPage } from "next/types";
+import * as React from "react";
+import {Box, Typography} from "@mui/material";
+import Head from "next/head";
+
+const About: NextPage = () => {
+    return(
+        <React.Fragment>
+            <Head>
+                <title>About - Cloudn</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+            </Head>
+            <Box className="flex justify-center items-center text-white z-2 backdrop-blur-0" sx={{height: '90vh', width: "auto"}}>
+                <Typography className="text-3xl md:text-5xl">
+                    About Page
+                </Typography>
+            </Box>
+        </React.Fragment>
+    );
+}
+
+export default About;
