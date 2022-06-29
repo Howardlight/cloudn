dddba7c2c5ccb6f0a939c8b35c7503e9c714279c
HowardLight <howardlight@protonmail.com>
2022-06-29 22:36:52 +0300

    Cleaned up [...geolocation]

--- pages/api/[...geolocation].ts
+++ pages/api/[...geolocation].ts
@@ -1,17 +1,11 @@
-import useSWR, { SWRResponse } from "swr";
-import { Weather, WeatherResponse } from "../../types";
-import { fetcher, useWeatherData } from "../../utils"
+import { WeatherResponse } from "../../types";
 import {NextApiResponse, NextApiRequest} from "next";
 
 //TODO: Add TS DOCs, which is first, lon or lat
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { geolocation } = req.query
 
-    // console.log(geolocation);
-
     let wReq: WeatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${geolocation[0]}&lon=${geolocation[1]}&appid=${process.env.OWA_API}`).then(data => data.json());
-    // wReq = wReq.json();
-    // console.log(wReq);
 
     //TODO: Make a response for code: 400, message: "wrong latitude"
 
